31c1dfdbd62763d8e2a240d67b54364d94488b40
T1ray <tester2016go@gmail.com>
2024-09-19 16:11:59 +0500

    testing commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function ButtonClick() {
     calculate();
     changeResult(res);
 }
-
+//Обновление
 function isNumber(str) {
     var flag = true;
     var nums = ['0','1','2','3','4','5','6','7','8','9'];
