82fefe5061ea431dd3db5c5399a36c889057ec58
IlyaPostdm <ipostnikov05@yandex.ru>
2024-09-25 18:38:52 +0500

    added comments

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+// Функция, выполняемая после надатия кнопки
 function ButtonClick() {
     saveValues();
     calculate();
     changeResult(res);
 }
 
+/*
+Эта функция вычисляет, какой тип данных в полях для ввода, она определяет несколько типов вхтодеых данных:
+    0 - нуль 
+    1 - отрицательные числа
+    2 - положительные числа
+    3 - вещественные числа
+    4 - комплексные числа
+    -1 - все то, что не подошло под нижеперечисленные категории
+*/
 function TypeNumber(str) {
     console.log(str);
     try {
@@ -12,11 +22,11 @@ function TypeNumber(str) {
     console.log(str);
     var flag = -1;
     var nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    if (str === "0") //zero
-    {
+    //Проверка на 0
+    if (str === "0") {
         flag = 0;
         return flag;
-    } else if (str[0] === "-") //negative number
+    } else if (str[0] === "-") //// Проверка, является ли число отрицательным
     {
         flag = 1;
         for (var i = 1; i < str.length; i++) {
@@ -26,7 +36,7 @@ function TypeNumber(str) {
             }
         }
 
-    } else //positive numbers
+    } else // Проверка, является ли число положиетльным
     {
         flag = 2;
         for (var i = 0; i < str.length; i++) {
@@ -37,10 +47,7 @@ function TypeNumber(str) {
         }
 
     }
-    try {
-        let x = parseFloat(str);
-        console.log(typeof x, x);
-    } catch (e) {}
+    // Проверка на 
     try {
         // Попытка преобразовать в комплексное число
         let complex = math.complex(str);
@@ -49,33 +56,41 @@ function TypeNumber(str) {
         if (complex.im !== 0) {
             flag = 4;
             return flag;
+            // Проверка, является ли число вещественным
         } else if (complex.im === 0 && !Number.isInteger(complex.re)) {
             flag = 3;
         }
     } catch (e) {}
-    // if (str === '' || isNaN(str)) {
-    //     console.log('float');
-    //     flag = -1;
-    // }
     return flag;
 }
 
+// Функция, которая считает ответ, в зависимости от типа входных данных
 function calculate() {
     var res = null;
     var TypeNumVal = TypeNumber(val);
     var TypeNumAccur = TypeNumber(accur);
+
+    // Попытка упростить введенные значения
     try {
         val = math.evaluate(val).toString().replace(' ', '');
     } catch (e) {}
     console.log(TypeNumVal, val);
-    //console.log(TypeNumAccur, accur);
+
+    //Для подходящих типов данных, счет результата
     if (TypeNumVal != -1 && (TypeNumAccur === 2 || TypeNumAccur === 0)) {
+        // Для нуля
         if (TypeNumVal === 0) {
             res = "0";
+
+            // Счет результата для положительных типов входных данных
         } else if (TypeNumVal === 2) {
             res = "±" + (Math.sqrt(val)).toFixed(accur);
+
+            // Счет результата для отрицательных типов входных данных
         } else if (TypeNumVal === 1) {
             res = "±" + (Math.sqrt(Math.abs(val))).toFixed(accur) + "i";
+
+            // Счет результата для вещественных типов входных данных
         } else if (TypeNumVal === 3) {
             let num = parseFloat(val);
             if (Math.sign(num) === 1) {
@@ -83,6 +98,8 @@ function calculate() {
             } else {
                 res = "±" + (Math.sqrt(Math.abs(val))).toFixed(accur) + "i";
             }
+
+            // Счет результата для комплексных типов входных данных
         } else {
             let ComplexNum = math.complex(val);
             let sqrtResult = math.sqrt(ComplexNum);
@@ -91,12 +108,14 @@ function calculate() {
             res = String(`${sqrtResult.re.toFixed(accur)}${sign}${Math.abs(sqrtResult.im).toFixed(accur)}i`)
         }
 
+        // Вывод в слачае ошибка
     } else {
         res = 'Invalid input format';
     }
     window.res = res;
 }
 
+// Функция для отоброжения полученного результата
 function changeResult(value) {
     const out = document.getElementById('en');
     out.value = String(value);
@@ -110,9 +129,6 @@ function saveValues() {
     let val = document.getElementById('en').value.replace(",", ".");
     let accur = document.getElementById('nod').value;
 
-    // Выводим значения в консоль (или выполняем другую логику)
-    //console.log(typeof val, "Значение val:", val);
-    //console.log(typeof accur, "Значение accur:", accur);
     // Добавляем возможность использовать val и accur вне функции
     window.val = val;
     window.accur = accur;
